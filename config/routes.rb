f7b5a010b9766f978bbf941b63b8fe64081d1746
Cooper Pell <cooper@backerkit.com>
2022-10-03 00:48:05 -0700

    added votes to topic description

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'topics#index'
-  resources :topics
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
