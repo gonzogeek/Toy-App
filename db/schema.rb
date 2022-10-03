595f2194c3449fd6b7927956ada996c2abba5979
Cooper Pell <cooper@backerkit.com>
2022-10-03 00:34:46 -0700

    added votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_065831) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_072057) do
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
